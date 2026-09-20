741bfb2d893d59d7fc913d9d76710ad6b9bfa9cd
Antonio Lazarte Anibarro <antonio.lazarte.a@gmail.com>
2026-09-19 22:23:48 -0400

    test: provocar fallo de arranque para simulacro de caos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
+throw new Error("fallo simulado");
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT} ...`);
 });
